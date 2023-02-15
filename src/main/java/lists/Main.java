23f4fd321b76bd9cf0cc455ec7c9f2ade1b26a1d
Kristina Muntian <kristinaflorentina01@gmail.com>
2023-02-15 15:18:56 +0100

    Test removeAt method

---
+++ src/main/java/lists/Main.java
@@ -0,0 +1,17 @@
+package lists;
+
+public class Main {
+    public static void main(String[] args) {
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.add(1);
+        dynamicArray.add(2);
+        dynamicArray.add(3);
+        dynamicArray.add(4);
+        dynamicArray.add(5);
+
+
+        dynamicArray.removeAt(3);
+
+        System.out.println(dynamicArray);
+    }
+}
